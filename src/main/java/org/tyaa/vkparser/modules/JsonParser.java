083f3657e4bd92b5af3d22e8d4b6e05f32211ff9
student <student@192.168.56.1>
2017-02-02 20:25:59 +0200

    add new method to JsonParser

--- src/main/java/org/tyaa/vkparser/modules/JsonParser.java
+++ src/main/java/org/tyaa/vkparser/modules/JsonParser.java
@@ -48,4 +48,37 @@ public class JsonParser
         
         return vKUser;
     }
+    
+    public VKUser parseVKGroup(String _jsonString){
+    
+        JSONObject dataJsonObj = null;
+        VKUser vKUser = new VKUser();
+
+        try {
+            dataJsonObj = new JSONObject(_jsonString);
+            JSONArray response = dataJsonObj.getJSONArray("response");
+
+            // 1. достаем индекс 0
+            JSONObject userDataJSONObject = response.getJSONObject(0);
+            vKUser.mInterests = userDataJSONObject.getString("interests");
+            vKUser.mActivities = userDataJSONObject.getString("activities");
+            vKUser.mAbout = userDataJSONObject.getString("about");
+             
+            // 2. перебираем и выводим контакты каждого друга
+            /*for (int i = 0; i < response.length(); i++) {
+                JSONObject friend = response.getJSONObject(i);
+
+                JSONObject contacts = friend.getJSONObject("contacts");
+
+                String phone = contacts.getString("mobile");
+                String email = contacts.getString("email");
+                String skype = contacts.getString("skype");
+            }*/
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        return vKUser;
+    }
 }
